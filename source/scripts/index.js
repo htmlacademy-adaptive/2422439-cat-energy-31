60e38d2a6ed13f9077329b774f72b71e85349a74
Liubov.Bogza <bogza.lubov@gmail.com>
2024-05-31 02:06:10 -0700

    Исправление замечаний

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,6 +1,13 @@
 /* в этот файл добавляет скрипты*/
+const header = document.querySelector('.header');
 const navMain = document.querySelector('.navigation');
 const navToggle = document.querySelector('.navigation__toggle');
+
+header.classList.remove('header--nojs');
 navMain.classList.remove('navigation--nojs');
 navMain.classList.remove('navigation--opened');
+
+const interactiveMap = document.querySelector('.contacts__map-iframe');
+interactiveMap.classList.remove('contacts__map-iframe--nojs');
+
 navToggle.addEventListener('click', () => navMain.classList.toggle('navigation--opened'));
